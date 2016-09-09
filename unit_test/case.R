f6e9673f78606ff0b53d8d207f543e47e5e9e8bc
JS Gourdet <js@jsdev.net>
2016-09-09 18:11:17 +0400

    Create case.R

---
+++ unit_test/case.R
@@ -0,0 +1,23 @@
+## UTF-8 with Latin Header
+
+## UTF-8 with Georgian header
+
+## UTF-8 BOM with Latin Header
+
+## UTF-8 BOM with Georgian Header
+
+## UTF-8 BOM with Latin Header and comma
+
+## UTF-8 BOM with Latin Header and dates
+
+
+## UCS-2LE BOM with Latin Header
+geo_read.csv("unicode/sample_UCS-2LE-BOM-lat-header.csv",method="UCS-2LE")
+
+## UCS-2LE BOM with Georgian Header
+geo_read.csv("unicode/sample_UCS-2LE-BOM-geo-header.csv",method="UCS-2LE",trsheader=T)
+
+## UCS-2LE BOM with Latin Header, date
+
+
+## UCS-2LE BOM with Latin Header, date, comma
