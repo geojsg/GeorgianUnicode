8a111868eb2ad08d8138ba90495b54a730927315
JS Gourdet <js@jsdev.net>
2016-09-10 13:53:41 +0400

    Update geo_csv.R

--- geo_csv.R
+++ geo_csv.R
@@ -1,11 +1,12 @@
 ### READ CSV UNICODE WITH GEORGIAN CHARACTERS
-geo_read.csv<-function(f,skip=0,header=T,method="UT8",colclass="character",trsheader=F) {
-	if (method=="UT8" | method=="BOM") {
+geo_read.csv<-function(f,skip=0,header=T,method="UTF-8",colclass="character",trsheader=F) {
+	if ((method=="UTF-8" | method=="UTF-8-BOM") & !trsheader) {
     table<-read.csv(f, encoding="UTF-8",skip=skip,header=header,colClasses=colclass)
-  	if(method=="BOM")	names(table)[1]<-substring(names(table)[1],10)
+  	if(method=="UTF-8-BOM")	names(table)[1]<-substring(names(table)[1],10)
 	}
   else {
-    A <- readLines(con <- file(f, encoding = "UCS-2LE"))
+    if (method=="UTF-8-BOM") method="UTF-8"
+    A <- readLines(con <- file(f, encoding = method))
     if(header) 
       {
         table<-read.table(text = A[-1], sep = ",")
